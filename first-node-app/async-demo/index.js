2f8f0891210b20544d0fbd984169ce2a9556ac01
david.crespo <david.crespo@hmcts.net>
2018-09-01 16:00:41 +0200

    option1 to improve the callback hell issue. Replace anonymous function

--- first-node-app/async-demo/index.js
+++ first-node-app/async-demo/index.js
@@ -1,13 +1,20 @@
 console.log('Before');
-
-getUser(1, (user) => {
-    console.log(user);
-    getRepos(user.gitUsername, (repos) => {
-        console.log(repos);
-    });
-});
-
+getUser(1, displayUser);
 console.log('After');
+
+function displayUser(user) {
+    console.log(user);
+    getRepos(user.gitUsername, displayRepos);
+}
+
+function displayRepos(repos) {
+    console.log(repos);
+    getCommits(repos[0], displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log(commits);
+}
 
 function getUser(id, callback) {
     setTimeout(() => {
@@ -23,5 +30,12 @@ function getRepos(username, callback) {
     setTimeout(() => {
         console.log(`getting repos for ${username}...`);
         callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+
+function getCommits(repo, callback) {
+    setTimeout(() => {
+        console.log(`getting commits for the ${repo} repo...`);
+        callback(['commit1', 'commit2', 'commit3']);
     }, 2000);
 }
