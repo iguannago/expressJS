7f4bb6c06d15bd54e20100b08e3676e6eff2d4d2
david.crespo <david.crespo@hmcts.net>
2018-09-01 02:24:32 +0200

    http module

---
+++ app-http.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World!');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+server.on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+        console.log('Address in use, retrying...');
+        setTimeout(() => {
+            server.close();
+            server.listen(3000);
+        }, 1000);
+    }
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
