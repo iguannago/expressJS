aac9a5c015606508e560cbe1a7d66df378da4d94
david.crespo <david.crespo@hmcts.net>
2018-09-02 18:58:43 +0200

    replace callback functions to return promises

--- first-node-app/async-demo-callback-fix-with-promises/index.js
+++ first-node-app/async-demo-callback-fix-with-promises/index.js
@@ -8,23 +8,32 @@ getUser(1, (user) => {
 });
 console.log('After');
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from a database...');
-    callback({ id: id, gitHubUsername: 'david' });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('getting user...');
+      resolve({
+        id: id,
+        gitHubUsername: 'david'
+      });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting repos...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
 
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['commit']);
-  }, 2000);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting commits...');
+      resolve(['commit']);
+    }, 2000);
+  });
 }
