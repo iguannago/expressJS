fb30879e72511e30bf0fc7f9c9f29a93c691afa8
david.crespo <david.crespo@hmcts.net>
2018-09-02 16:53:57 +0200

    promise with then and catch

--- first-node-app/promises/promise.js
+++ first-node-app/promises/promise.js
@@ -1,9 +1,10 @@
 const p = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(1);
+        // resolve(1);
+        reject(new Error('there was an error!'));
     }, 2000);
 });
 
 p.then(result => {
     console.log(`Result: ${result}`);
-})
+}).catch(err => console.log(err));
