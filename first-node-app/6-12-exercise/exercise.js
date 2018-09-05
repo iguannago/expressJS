46b5a568a312bd39cea33d0e1be48acfca2f926b
david.crespo <david.crespo@hmcts.net>
2018-09-06 00:55:23 +0100

    resolve exercise using promises

--- first-node-app/6-12-exercise/exercise.js
+++ first-node-app/6-12-exercise/exercise.js
@@ -1,36 +1,60 @@
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
-  }
-});
+// getCustomer(1, (customer) => {
+//   console.log('Customer: ', customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log('Top movies: ', movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log('Email sent...')
+//       });
+//     });
+//   }
+// });
+
+
+// exercise done using promises and handling errors as well.
+getCustomer(1)
+  .then((customer) => {
+    if (customer.isGold) {
+      getTopMovies()
+        .then(movies => sendEmail(customer.email, movies))
+        .then(result => console.log(`email: ${result.email}, movies: ${result.movies}`))
+        .catch(err => console.log(err));
+    }
+  })
+  .catch(err => console.log(err));
 
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      console.log('getting customer...');
       resolve({
         id: 1,
         name: 'David Crespo',
         isGold: true,
-        email: 'email'
+        email: 'davidcrespoarroyo@gmail.com'
       });
     }, 4000);
   });
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('getting top movies...');
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('sending email...');
+      // resolve({
+      //   email: email,
+      //   movies: movies
+      // });
+      reject(new Error('error when sending email....'));
+    }, 4000);
+  });
 }
