6e2ad2e156166a77c096249544ee8136ca83e0c9
david.crespo <david.crespo@hmcts.net>
2018-09-05 23:25:20 +0100

    add log to show promise is asynchronous

--- first-node-app/promises/promise.js
+++ first-node-app/promises/promise.js
@@ -1,10 +1,14 @@
 const p = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve(1);
-        reject(new Error('there was an error!'));
-    }, 2000);
+        resolve(1);
+        // reject(new Error('there was an error!'));
+    }, 6000);
 });
+
+console.log('*** 1')
 
 p.then(result => {
     console.log(`Result: ${result}`);
 }).catch(err => console.log(err));
+
+console.log('*** 2');
