4e66166cd8963e53d378aad5f458fdabf87a3d04
david.crespo <david.crespo@hmcts.net>
2018-09-11 02:06:44 +0200

    Add get route to get a specific course

--- first-node-app/express-demo/index.js
+++ first-node-app/express-demo/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
-})
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
