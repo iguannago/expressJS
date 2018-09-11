b8305e5b43f626becb60de35f420ac96814ddddb
david.crespo <david.crespo@hmcts.net>
2018-09-11 02:47:46 +0200

    get a given course by id and handlen 404 error

--- first-node-app/express-demo/index.js
+++ first-node-app/express-demo/index.js
@@ -5,12 +5,30 @@ app.get('/', (req, res) => {
     res.send('Hello World!!!!');
 });
 
+const courses = [{
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },
+    {
+        id: 3,
+        name: 'course3'
+    }
+];
+
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
+
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
+    res.send(course);
+
 });
 
 app.get('/api/post/:year/:month', (req, res) => {
