9f902a0f61c63ffb21b8858cd414b08e09228ad8
david.crespo <david.crespo@hmcts.net>
2018-09-14 01:03:46 +0200

    add get genres endpoint

--- first-node-app/express-demo/vidly-mine/index.js
+++ first-node-app/express-demo/vidly-mine/index.js
@@ -3,3 +3,18 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on port ${port}...`));
+
+const genres = [
+    { id: 1, genre: 'comedy' },
+    { id: 2, genre: 'love' },
+    { id: 3, genre: 'romantic' },
+    { id: 4, genre: 'terror' }
+];
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+
+});
