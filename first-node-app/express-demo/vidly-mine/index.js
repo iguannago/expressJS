39fe2d8c9c0b9303c58e41865948bae341ca4820
david.crespo <david.crespo@hmcts.net>
2018-09-15 00:42:25 +0200

    Add put endpoint and fix post one

--- first-node-app/express-demo/vidly-mine/index.js
+++ first-node-app/express-demo/vidly-mine/index.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on port ${port}...`));
 
 const genres = [
-    { id: 1, genre: 'comedy' },
-    { id: 2, genre: 'love' },
-    { id: 3, genre: 'romantic' },
-    { id: 4, genre: 'terror' }
+    { id: 1, name: 'comedy' },
+    { id: 2, name: 'love' },
+    { id: 3, name: 'romantic' },
+    { id: 4, name: 'terror' }
 ];
 
 app.get('/api/genres', (req, res) => {
@@ -20,21 +20,43 @@ app.get('/api/genres', (req, res) => {
 
 app.get('/api/genres/:id', (req, res) => {
     const id = req.params.id;
-    const genre = genres.find(g => g.id === parseInt(id));
-    if (!genre) return res.status(404).send(`Course not found for given id(${id})`);
+    const genre = findCourseByid(id);
+    return404IfCourseNotFound(genre, id, res);
     res.send(genre);
 });
 
 app.post('/api/genres/', (req, res) => {
-    const schema = Joi.object().keys({
-        genre: Joi.string().min(3).required()
-    });
-    const { error } = Joi.validate(req.body, schema);
-    if (error) return res.status(40).send(error);
+    const { error } = validateBody(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     const genre = {
         id: genres.length + 1,
-        genre: req.body.genre
+        name: req.body.name
     };
     genres.push(genre);
     res.send(genre)
 });
+
+app.put('/api/genres/:id', (req, res) => {
+    const id = req.params.id;
+    const genre = findCourseByid(id);
+    return404IfCourseNotFound(genre, id, res);
+    const { error } = validateBody(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    genre.name = req.body.name;
+    res.send(genre)
+});
+
+function validateBody(body) {
+    const schema = Joi.object().keys({
+        name: Joi.string().min(3).required()
+    });
+    return Joi.validate(body, schema);
+}
+
+function findCourseByid(id) {
+    return genres.find(g => g.id === parseInt(id));
+}
+
+function return404IfCourseNotFound(genre, id, res) {
+    if (!genre) return res.status(404).send(`Course not found for given id(${id})`);
+}
