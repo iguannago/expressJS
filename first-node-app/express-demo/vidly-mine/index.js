548cbade74939f1796800db2450c1a7a114302c1
david.crespo <david.crespo@hmcts.net>
2018-09-14 21:02:45 +0200

    Add post endpoint and validation using Joi

--- first-node-app/express-demo/vidly-mine/index.js
+++ first-node-app/express-demo/vidly-mine/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on port ${port}...`));
@@ -20,4 +23,18 @@ app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(id));
     if (!genre) return res.status(404).send(`Course not found for given id(${id})`);
     res.send(genre);
+});
+
+app.post('/api/genres/', (req, res) => {
+    const schema = Joi.object().keys({
+        genre: Joi.string().min(3).required()
+    });
+    const { error } = Joi.validate(req.body, schema);
+    if (error) return res.status(40).send(error);
+    const genre = {
+        id: genres.length + 1,
+        genre: req.body.genre
+    };
+    genres.push(genre);
+    res.send(genre)
 });
