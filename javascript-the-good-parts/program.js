021e3cc8079a4fbd60701afe3374c062433c460f
david.crespo <david.crespo@hmcts.net>
2018-09-16 10:00:32 +0200

    objects

--- javascript-the-good-parts/program.js
+++ javascript-the-good-parts/program.js
@@ -1,5 +1,17 @@
-document.writeln('Hello, world!!!');
-
-const var1 = "hello, ", var2 = "world!!!";
+const var1 = "hello, ",
+    var2 = "world!!!";
 
 document.writeln(var1 + var2);
+
+const object = {
+    id: "1",
+    first_name: "david",
+    "second-name": "crespo",
+    city: "London",
+};
+
+document.writeln(JSON.stringify(object));
+document.writeln(JSON.stringify(object.id));
+document.writeln(JSON.stringify(object.first_name));
+document.writeln(JSON.stringify(object["second-name"]));
+document.writeln(JSON.stringify(object.city));
