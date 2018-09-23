353f5f93fc0867349e7772260fe30b23f7892b73
david.crespo <david.crespo@hmcts.net>
2018-09-24 00:14:37 +0200

    refactor code and move routes to separate folder and file

---
+++ first-node-app/express-demo/routes/courses.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+
+const courses = [{
+        id: 1,
+        name: 'course1'
+    },
+    {
+        id: 2,
+        name: 'course2'
+    },
+    {
+        id: 3,
+        name: 'course3'
+    }
+];
+
+router.get('/', (req, res) => {
+    res.send(courses);
+});
+
+router.get('/:id', (req, res) => {
+    const course = findCourseById(req.params.id);
+    if (!course) return res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
+    res.send(course);
+});
+
+router.post('/', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(validationResult.error.details[0].message);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+router.put('/:id', (req, res) => {
+    const course = findCourseById(req.params.id);
+    if (!course) return res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    course.name = req.body.name;
+    res.send(course);
+});
+
+router.delete('/:id', (req, res) => {
+    const course = findCourseById(req.params.id);
+    if (!course) return res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+});
+
+function findCourseById(id) {
+    return courses.find(c => c.id === parseInt(id));
+}
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
+
+module.exports = router;
