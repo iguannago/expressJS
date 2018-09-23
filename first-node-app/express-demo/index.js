0283876c45e93fbbf30264b65701c4fa4f677f6b
david.crespo <david.crespo@hmcts.net>
2018-09-24 00:14:24 +0200

    refactor code and move routes to separate folder and file

--- first-node-app/express-demo/index.js
+++ first-node-app/express-demo/index.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const courses = require('./routes/courses');
 const express = require('express');
 const logger = require('./logger.js');
 const authentication = require('./authentication.js');
@@ -9,9 +10,16 @@ const appStartupDebugger = require('debug')('app:startup');
 const appDbDebugger = require('debug')('app:db');
 const app = express();
 
+if (app.get('env') === 'development') {
+    app.use(logger);
+    app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+    appStartupDebugger('Morgan enabled...');
+    appDbDebugger('Connected to DB...')
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/courses', courses);
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -20,12 +28,6 @@ console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`app.get: ${app.get('env')}`); //if NODE_EN is not defined falls back to development
 app.use(express.static('public'));
 
-if (app.get('env') === 'development') {
-    app.use(logger);
-    app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
-    appStartupDebugger('Morgan enabled...');
-    appDbDebugger('Connected to DB...')
-}
 app.use(authentication);
 app.use(helmet());
 
@@ -37,73 +39,5 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Express App', message: 'hello, world!!!' });
 });
 
-const courses = [{
-        id: 1,
-        name: 'course1'
-    },
-    {
-        id: 2,
-        name: 'course2'
-    },
-    {
-        id: 3,
-        name: 'course3'
-    }
-];
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = findCourseById(req.params.id);
-    if (!course) return res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
-    res.send(course);
-});
-
-app.get('/api/post/:year/:month', (req, res) => {
-    res.send(req.query);
-});
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validateCourse(req.body);
-    if (error) return res.status(400).send(validationResult.error.details[0].message);
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = findCourseById(req.params.id);
-    if (!course) return res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
-    const { error } = validateCourse(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    course.name = req.body.name;
-    res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = findCourseById(req.params.id);
-    if (!course) return res.status(404).send(`The course with the given ID(${req.params.id}) is not found`);
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-    res.send(course);
-});
-
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
-
-function findCourseById(id) {
-    return courses.find(c => c.id === parseInt(id));
-}
-
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(course, schema);
-}
