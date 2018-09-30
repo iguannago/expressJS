c198e920b508bb2c430fe0a14f9a1d8490d43e63
david.crespo <david.crespo@hmcts.net>
2018-09-30 17:39:55 +0200

    restructure the routes

--- first-node-app/express-demo/vidly-mine/index.js
+++ first-node-app/express-demo/vidly-mine/index.js
@@ -1,71 +1,10 @@
 const express = require('express');
 const app = express();
 const Joi = require('joi');
+const genres = require('./routes/genres');
 
 app.use(express.json());
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on port ${port}...`));
-
-const genres = [
-    { id: 1, name: 'comedy' },
-    { id: 2, name: 'love' },
-    { id: 3, name: 'romantic' },
-    { id: 4, name: 'terror' }
-];
-
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-    const id = req.params.id;
-    const genre = findCourseByid(id);
-    return404IfCourseNotFound(genre, id, res);
-    res.send(genre);
-});
-
-app.post('/api/genres/', (req, res) => {
-    const { error } = validateBody(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    };
-    genres.push(genre);
-    res.send(genre)
-});
-
-app.put('/api/genres/:id', (req, res) => {
-    const id = req.params.id;
-    const genre = findCourseByid(id);
-    return404IfCourseNotFound(genre, id, res);
-    const { error } = validateBody(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    genre.name = req.body.name;
-    res.send(genre)
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-    const id = req.params.id;
-    const genre = findCourseByid(id);
-    return404IfCourseNotFound(genre, id, res);
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-    res.send(genre);
-});
-
-function validateBody(body) {
-    const schema = Joi.object().keys({
-        name: Joi.string().min(3).required()
-    });
-    return Joi.validate(body, schema);
-}
-
-function findCourseByid(id) {
-    return genres.find(g => g.id === parseInt(id));
-}
-
-function return404IfCourseNotFound(genre, id, res) {
-    if (!genre) return res.status(404).send(`Course not found for given id(${id})`);
-}
