b35485890d4125533c36e7167c571ca639fa8c1e
david.crespo <david.crespo@hmcts.net>
2018-10-28 22:06:41 +0100

    query y documents

--- first-node-app/mongo-demo/index.js
+++ first-node-app/mongo-demo/index.js
@@ -16,7 +16,7 @@ const courseSchema = mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
-createCourse();
+// createCourse();
 
 async function createCourse() {
     const course = new Course({
@@ -28,3 +28,14 @@ async function createCourse() {
     const result = await course.save();
     debug(result);
 }
+
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'David', isPublished: false })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 })
+    debug(courses);
+}
+
+getCourses();
