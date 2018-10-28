ca7200a75f195294e0d03e3a2a34cda8b8cfcc29
david.crespo <david.crespo@hmcts.net>
2018-10-28 21:51:31 +0100

    Create schema, model and document

--- first-node-app/mongo-demo/index.js
+++ first-node-app/mongo-demo/index.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 const debug = require('debug')('mongo');
 
-mongoose.connect('mongodb://127.0.0.1/playground')
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
     .then(() => debug('connected to mongoDB ...'))
     .catch(err => debug('Cannot connecto to mongoDB...', new Error(err)));
+
+
+const courseSchema = mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+createCourse();
+
+async function createCourse() {
+    const course = new Course({
+        name: 'angular course',
+        author: 'David',
+        tags: ['nodejs', 'beginner'],
+        isPublished: false
+    });
+    const result = await course.save();
+    debug(result);
+}
